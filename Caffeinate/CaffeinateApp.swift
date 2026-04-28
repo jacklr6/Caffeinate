81dea7d6a149c327c531e9731761a8fbeee064c3
jacklr6 <captapplesauce6@gmail.com>
2026-04-27 21:43:07 -0400

    Initial Commit

---
+++ Caffeinate/CaffeinateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaffeinateApp.swift
+//  Caffeinate
+//
+//  Created by Jack Rogers on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct CaffeinateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
